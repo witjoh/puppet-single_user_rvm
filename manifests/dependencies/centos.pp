7d81a0b992e8fed378067fb3d1d853fa9018b2bf
Cosmina Chindris <cosmina.chindris@wizecommerce.com>
2014-03-12 11:47:47 -0700

    added more dependencies to centos

--- manifests/dependencies/centos.pp
+++ manifests/dependencies/centos.pp
@@ -10,6 +10,13 @@ class single_user_rvm::dependencies::centos inherits single_user_rvm{
 
   if ! defined(Package['curl']) { package { 'curl': ensure => present } }
   if ! defined(Package['git']) { package { 'git': ensure => present } }
+  # RVM dependencies
+  if ! defined(Package['bash'])            { package { 'bash':            ensure => present } }
+  if ! defined(Package['curl'])            { package { 'curl':            ensure => present } }
+  if ! defined(Package['patch'])           { package { 'patch':           ensure => present } }
+  if ! defined(Package['bzip2'])           { package { 'bzip2':           ensure => present } }
+  if ! defined(Package['ca-certificates']) { package { 'ca-certificates': ensure => present } }
+  if ! defined(Package['gawk'])            { package { 'gawk':            ensure => present } }
 
   if ! defined(Package['which']) { package { 'which': ensure => present } }
   if ! defined(Package['gcc']) { package { 'gcc': ensure => present } }
@@ -18,7 +25,6 @@ class single_user_rvm::dependencies::centos inherits single_user_rvm{
   if ! defined(Package['libxslt']) { package { 'libxslt': ensure => present } }
   if ! defined(Package['libxslt-devel']) { package { 'libxslt-devel': ensure => present } }
   if ! defined(Package['gcc-c++']) { package { 'gcc-c++': ensure => present } }
-  if ! defined(Package['patch']) { package { 'patch': ensure => present } }
   if ! defined(Package['readline']) { package { 'readline': ensure => present } }
   if ! defined(Package['readline-devel']) { package { 'readline-devel': ensure => present } }
   if ! defined(Package['zlib']) { package { 'zlib': ensure => present } }
@@ -27,7 +33,6 @@ class single_user_rvm::dependencies::centos inherits single_user_rvm{
   if ! defined(Package['libffi-devel']) { package { 'libffi-devel': ensure => present } }
   if ! defined(Package['openssl-devel']) { package { 'openssl-devel': ensure => present } }
   if ! defined(Package['make']) { package { 'make': ensure => present } }
-  if ! defined(Package['bzip2']) { package { 'bzip2': ensure => present } }
   if ! defined(Package['autoconf']) { package { 'autoconf': ensure => present } }
   if ! defined(Package['automake']) { package { 'automake': ensure => present } }
   if ! defined(Package['libtool']) { package { 'libtool': ensure => present } }

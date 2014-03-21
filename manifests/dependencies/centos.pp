ecf045b4ed7718021da831cf4f0fabbd4c46cded
Cosmina Chindris <cosmina.chindris@wizecommerce.com>
2014-03-21 11:28:45 -0700

    added the openjdk java 7 in supporting jruby installation with rvm

--- manifests/dependencies/centos.pp
+++ manifests/dependencies/centos.pp
@@ -10,6 +10,7 @@ class single_user_rvm::dependencies::centos inherits single_user_rvm{
 
   if ! defined(Package['curl']) { package { 'curl': ensure => present } }
   if ! defined(Package['git']) { package { 'git': ensure => present } }
+  if ! defined(Package['wget']) { package { 'wget': ensure => present } }
   # RVM dependencies
   if ! defined(Package['bash'])            { package { 'bash':            ensure => present } }
   if ! defined(Package['curl'])            { package { 'curl':            ensure => present } }
@@ -20,23 +21,23 @@ class single_user_rvm::dependencies::centos inherits single_user_rvm{
 
   if ! defined(Package['which']) { package { 'which': ensure => present } }
   if ! defined(Package['gcc']) { package { 'gcc': ensure => present } }
+  if ! defined(Package['gcc-c++']) { package { 'gcc-c++': ensure => present } }
+  if ! defined(Package['make']) { package { 'make': ensure => present } }
   if ! defined(Package['libxml2']) { package { 'libxml2': ensure => present } }
   if ! defined(Package['libxml2-devel']) { package { 'libxml2-devel': ensure => present } }
   if ! defined(Package['libxslt']) { package { 'libxslt': ensure => present } }
   if ! defined(Package['libxslt-devel']) { package { 'libxslt-devel': ensure => present } }
-  if ! defined(Package['gcc-c++']) { package { 'gcc-c++': ensure => present } }
   if ! defined(Package['readline']) { package { 'readline': ensure => present } }
   if ! defined(Package['readline-devel']) { package { 'readline-devel': ensure => present } }
   if ! defined(Package['zlib']) { package { 'zlib': ensure => present } }
   if ! defined(Package['zlib-devel']) { package { 'zlib-devel': ensure => present } }
-  if ! defined(Package['libyaml-devel']) { package { 'libyaml-devel': ensure => present } }
+#  if ! defined(Package['libyaml-devel']) { package { 'libyaml-devel': ensure => present } }
   if ! defined(Package['libffi-devel']) { package { 'libffi-devel': ensure => present } }
   if ! defined(Package['openssl-devel']) { package { 'openssl-devel': ensure => present } }
-  if ! defined(Package['make']) { package { 'make': ensure => present } }
   if ! defined(Package['autoconf']) { package { 'autoconf': ensure => present } }
   if ! defined(Package['automake']) { package { 'automake': ensure => present } }
   if ! defined(Package['libtool']) { package { 'libtool': ensure => present } }
   if ! defined(Package['bison']) { package { 'bison': ensure => present } }
   if ! defined(Package['glibc']) { package { 'glibc': ensure => present } }
-
+  if ! defined(Package['java-1.7.0-openjdk']) { package { 'java-1.7.0-openjdk': ensure => present } }
 }
